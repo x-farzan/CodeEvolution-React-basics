c9f2df9a3b41e9d6f55581702cfd20b567970e3d
x-farzan <farzanhassan245@gmail.com>
2023-01-31 11:39:48 +0500

    Initial commit

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+
+// Functional component
+
+// function Greet(){
+//     return <h1>Hi, Farzan!</h1>
+// }
+
+const Greet = () =>  <h1>Hi, I am Farzan in functional component!</h1>
+
+export default Greet;
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Welcome extends Component{
+    render(){
+        return <h1>Hi, I am Farzan in class component</h1>
+    }
+}
+
+export default Welcome;
