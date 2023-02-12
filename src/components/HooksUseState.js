ef4c2cbcf954cedf1335138c0c3cb4e5ea8fe1bb
x-farzan <farzanhassan245@gmail.com>
2023-02-13 01:46:36 +0500

    useState hook.

--- src/components/HooksUseState.js
+++ src/components/HooksUseState.js
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 
 function HooksUseState() {
-  const [state, setState] = useState(0);
+  const inititalStage = 0;
+  const [state, setState] = useState(inititalStage);
 
-  return <div>
+  function incrementFive() {
+    for (let i = 0; i < 5; i++) {
+      setState((prevState) => prevState + 1);
+    }
+  }
+
+  return (
+    <div>
       <h1>{state}</h1>
-      <button onClick={() => setState(state + 1)}>Increase counter</button>
-  </div>;
+      <button onClick={() => setState(inititalStage)}>Reset counter</button>
+      <button onClick={() => setState((prevState) => prevState + 1)}>
+        Increase counter
+      </button>
+      <button onClick={() => setState((prevState) => prevState - 1)}>
+        Decrease counter
+      </button>
+      <button onClick={incrementFive}>Increase counter by 5</button>
+    </div>
+  );
 }
 
 export default HooksUseState;
