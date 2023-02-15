0f7eda3850f2b4e458f20daef34d57d1256a5b67
Farzan98 <farzan.hassan@developers.studio>
2023-02-15 18:09:10 +0500

    useReducer() with objects.

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -6,15 +6,17 @@ import React, { useReducer } from "react";
 //      3.  const [ state, dispatch ] = useReducer(reducer, initialState)
 
 // Step 2
-const initialState = 0;
+const initialState = {
+  initialCount: 0,
+};
 const reducer = (state, action) => {
-  switch (action) {
+  switch (action.type) {
     case "increment":
-      return state + 1;
+      return state + action.increment;
     case "decrement":
-      return state - 1;
+      return state - action.increment;
     case "reset":
-      return initialState;
+      return initialState.initialCount;
     default:
       return state;
   }
@@ -22,13 +24,17 @@ const reducer = (state, action) => {
 
 function UseReducer() {
   // Step 1 and 3
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState.initialCount);
   return (
     <div>
       <h1>Count - {state}</h1>
-      <button onClick={() => dispatch("increment")}>Increment</button>
-      <button onClick={() => dispatch("decrement")}>Decrement</button>
-      <button onClick={() => dispatch("reset")}>Reset</button>
+      <button onClick={() => dispatch({ type: "increment", increment: 5 })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", increment: 1 })}>
+        Decrement
+      </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
 }
