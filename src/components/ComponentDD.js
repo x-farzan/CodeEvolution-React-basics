9a106e226fd7691cfd6c959649feecff5a823263
x-farzan <farzanhassan245@gmail.com>
2023-02-16 23:54:36 +0500

    useReducer with useContext.

--- src/components/ComponentDD.js
+++ src/components/ComponentDD.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { CounterContext } from "../App";
 
 function ComponentDD() {
+  const counterContext = useContext(CounterContext);
+
   return (
     <div>
-      ComponentDD
+      CoponentDD : Count - {counterContext.counterState}  
+      <button onClick={() => counterContext.counterDispatch("increment")}>
+        Increment
+      </button>
+      <button onClick={() => counterContext.counterDispatch("decrement")}>
+        Decrement
+      </button>
+      <button onClick={() => counterContext.counterDispatch("reset")}>
+        Reset
+      </button>
     </div>
-  )
+  );
 }
 
-export default ComponentDD
+export default ComponentDD;
--- src/components/ComponentFF.js
+++ src/components/ComponentFF.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { CounterContext } from "../App";
 
 function ComponentFF() {
+  const counterContext = useContext(CounterContext);
+
   return (
     <div>
-      ComponentFF
+      CoponentFF : Count - {counterContext.counterState}
+      <button onClick={() => counterContext.counterDispatch("increment")}>
+        Increment
+      </button>
+      <button onClick={() => counterContext.counterDispatch("decrement")}>
+        Decrement
+      </button>
+      <button onClick={() => counterContext.counterDispatch("reset")}>
+        Reset
+      </button>
     </div>
-  )
+  );
 }
 
-export default ComponentFF
+export default ComponentFF;
